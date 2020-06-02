400e192c6d9e5eefaff98b382cec0e4422cd531a
Alvaro Pardo <alvaropardo@live.com>
2020-06-01 22:00:17 -0300

    First Configuration and connection.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,13 +23,45 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.console_output_rt = new System.Windows.Forms.RichTextBox();
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // console_output_rt
+            // 
+            this.console_output_rt.Location = new System.Drawing.Point(12, 12);
+            this.console_output_rt.Name = "console_output_rt";
+            this.console_output_rt.Size = new System.Drawing.Size(367, 321);
+            this.console_output_rt.TabIndex = 2;
+            this.console_output_rt.Text = "";
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Location = new System.Drawing.Point(12, 350);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(367, 23);
+            this.connect_btn.TabIndex = 3;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(391, 385);
+            this.Controls.Add(this.connect_btn);
+            this.Controls.Add(this.console_output_rt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox console_output_rt;
+        private System.Windows.Forms.Button connect_btn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Discord;
+using Discord.WebSocket;
 
 namespace Vorgari {
     public partial class Form1: Form {
+
+        private DiscordSocketClient _client;
         public Form1() {
             InitializeComponent();
+        }
+
+        public async Task StartAsync() {
+
+        }
+
+        private async void connect_btn_Click(object sender, EventArgs e) {
+            _client = new DiscordSocketClient(new DiscordSocketConfig() {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            _client.Log += Client_Log;
+
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("VORGARI_BOT"));
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Log(LogMessage msg) {
+            Invoke((Action)delegate {
+                console_output_rt.AppendText(msg + "\n");
+            });
+            return null;
         }
     }
 }
