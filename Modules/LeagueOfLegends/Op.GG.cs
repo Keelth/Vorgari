5bd84b759f19af38e3bddb2edc3f2effa257196f
Alvaro Pardo <alvaropardo@live.com>
2020-06-04 02:15:30 -0300

    Fixed Issue of accounts created on same season with "lol.stats" command, added possibility to search on different servers.

--- Modules/LeagueOfLegends/Op.GG.cs
+++ Modules/LeagueOfLegends/Op.GG.cs
@@ -8,49 +8,57 @@ using System.Net.Http;
 using HtmlAgilityPack;
 using System.Drawing;
 using Discord;
+using System.Text.RegularExpressions;
 
 namespace Vorgari.Modules.LeagueOfLegends {
     public class Op: ModuleBase<SocketCommandContext> {
 
         [Command("lol.stats")]
-        public async Task LoLStatsAsync([Remainder] string user) {
-            string userName, level, ladder, ranked, rank, lp, win, lose, wl, wRatio, league, thumbnail, url;
+        public async Task LoLStatsAsync(string server, [Remainder] string user) {
+            string level, ladder, ranked, rank, lp, win, lose, wl, wRatio, league, thumbnail, url;
             HtmlWeb webP = new HtmlWeb();
-            HtmlDocument doc = webP.Load("https://las.op.gg/summoner/userName=" + user);
-            /*var htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div[2]/div/div/div/div[3]/div/span");
-            userName = htmlNode.InnerText.Trim();*/
-            var htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div/div[2]");
-            level = htmlNode.InnerText.Trim();
-            htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/div[2]/div/div/div/div[3]/div/div");
-            ladder = htmlNode.InnerText.Trim();
+            HtmlDocument doc = webP.Load("https://" + server + ".op.gg/summoner/userName=" + user);
+            var htmlNode = doc.DocumentNode.SelectSingleNode("/html");
+            Regex regex = new Regex("<span class=\"Level tip\" title=\"Level\">(.*?)</span>");
+            var v = regex.Match(htmlNode.OuterHtml);
+            level = v.Groups[1].ToString();
+            regex = new Regex("<span class=\"ranking\">(.*?)</span>");
+            v = regex.Match(htmlNode.OuterHtml);
+            ladder = "Ladder Rank " + v.Groups[1].ToString();
             ranked = "Ranked Solo/Duo";
-            htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div[2]");
-            rank = htmlNode.InnerText.Trim();
+            regex = new Regex("<div class=\"TierRank\">(.*?)</div>");
+            v = regex.Match(htmlNode.OuterHtml);
+            rank = v.Groups[1].ToString();
             htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div/span");
             lp = htmlNode.InnerText.Trim();
-            htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div/span[2]/span");
-            win = htmlNode.InnerText.Trim();
-            htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div/span[2]/span[2]");
-            lose = htmlNode.InnerText.Trim();
+            htmlNode = doc.DocumentNode.SelectSingleNode("/html");
+            regex = new Regex("<span class=\"wins\">(.*?)</span>");
+            v = regex.Match(htmlNode.OuterHtml);
+            win = v.Groups[1].ToString();
+            regex = new Regex("<span class=\"losses\">(.*?)</span>");
+            v = regex.Match(htmlNode.OuterHtml);
+            lose = v.Groups[1].ToString();
             wl = win + "/" + lose;
-            htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div[3]/span[2]/span[3]");
-            wRatio = htmlNode.InnerText.Trim();
+            regex = new Regex("<span class=\"winratio\">(.*?)</span>");
+            v = regex.Match(htmlNode.OuterHtml);
+            wRatio = v.Groups[1].ToString();
             htmlNode = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div[5]/div[2]/div/div/div/div[2]/div[4]");
             league = htmlNode.InnerText.Trim();
-            league = league.Replace("&#039;","'");
-            var htmlNodes = doc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/div/div/div/div[2]/div/img");
-            thumbnail = htmlNodes.OuterHtml.Substring(12);
-            thumbnail = thumbnail.Remove(thumbnail.Length - 23);
+            league = league.Replace("&#039;", "'");
+            htmlNode = doc.DocumentNode.SelectSingleNode("/html");
+            regex = new Regex("<img src=\"//(.*?)\" class=\"ProfileImage\">");
+            v = regex.Match(htmlNode.OuterHtml);
+            thumbnail = v.Groups[1].ToString();
             if(user.Split(' ').Length > 1) {
                 string newUser = "";
                 for (int i = 0; i < user.Split(' ').Length; i++) {
                     newUser += user.Split(' ')[i] + "+";
                 }
                 newUser = newUser.Remove(newUser.Length-1);
-                url = "https://las.op.gg/summoner/userName=" + newUser;
+                url = "https://" + server + ".op.gg/summoner/userName=" + newUser;
 
             } else {
-                url = "https://las.op.gg/summoner/userName=" + user;
+                url = "https://" + server + ".op.gg/summoner/userName=" + user;
             }
             var builder = new EmbedBuilder()
                     .WithTitle($"{user}")
@@ -61,7 +69,7 @@ namespace Vorgari.Modules.LeagueOfLegends {
                         footer
                             .WithText("Vorgari");
                     })
-                    .WithThumbnailUrl($"http://{thumbnail}")
+                    .WithThumbnailUrl($"https://{thumbnail}")
                     .AddField($"Level {level}", $"{ladder}")
                     .AddField($"{ranked}", $"{rank}")
                     .AddField($"{lp}", $"{wl}")
